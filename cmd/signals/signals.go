fbb2403d311b83e43bb085f435ffee82c9e5d28b
Tilmann Seiffert <mail@tilseiffert.de>
2023-09-29 16:52:25 +0200

    add descriptions
    Signed-off-by: Tilmann Seiffert <mail@tilseiffert.de>

--- cmd/signals/signals.go
+++ cmd/signals/signals.go
@@ -1,5 +1,17 @@
 package main
 
+/*
+The Go program serves as an example for managing context and handling system signals.
+At its core, it consists of three main routines: `main`, `handleSignals`, and `doSomething`.
+
+1. The `main` function sets up a cancellable context and initiates the other two routines as goroutines.
+2. `handleSignals` listens for system signals like `SIGTERM` and `SIGINT` to gracefully shut down the application by cancelling the context.
+3. `doSomething` simulates a long-running task that can be interrupted based on the context's state.
+
+The program utilizes a `WaitGroup` to ensure that all goroutines complete their execution before the program exits.
+This example effectively demonstrates how to use context for cancellation and how to handle system signals for a graceful shutdown.
+*/
+
 // Import required packages
 import (
 	"context"   // For managing timeouts and cancellation signals
