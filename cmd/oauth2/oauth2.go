162d0d9e10a8fb837617c2eb725d5c926d542f64
Tilmann Seiffert <mail@tilseiffert.de>
2023-10-19 23:18:38 +0200

    comments
    Signed-off-by: Tilmann Seiffert <mail@tilseiffert.de>

--- cmd/oauth2/oauth2.go
+++ cmd/oauth2/oauth2.go
@@ -22,96 +22,100 @@ const (
 type session struct {
 	config   oauth2.Config
 	provider *oidc.Provider
+	ctx      context.Context
 }
 
 // handleAuthCallback handles the OAuth2 callback
 func (s *session) handleAuthCallback(w http.ResponseWriter, r *http.Request) {
-	// Implementiere den Callback für OAuth2
 	log.Println("### Callback called...")
 
+	// Extract authorization code from URL query
 	code := r.URL.Query().Get("code")
 	if code == "" {
-		// Fehlerbehandlung
+		// Handle missing code
 		w.WriteHeader(http.StatusBadRequest)
 		_, err := w.Write([]byte("Code not found in request"))
-
 		if err != nil {
 			log.Println("Error writing response:", err)
 		}
+		return
 	}
 
 	log.Println("Code:", code)
 
-	ctx := context.Background()
-
-	token, err := s.config.Exchange(ctx, code)
+	// Perform OAuth2 code exchange
+	token, err := s.config.Exchange(s.ctx, code)
 	if err != nil {
-		// Fehlerbehandlung
+		// Handle exchange error
 		log.Fatalln("token exchange failed:", err)
-
 		w.WriteHeader(http.StatusBadRequest)
 		_, err := w.Write([]byte("Token exchange failed"))
-
 		if err != nil {
 			log.Println("Error writing response:", err)
 		}
+		return
 	}
 
 	log.Println("Token:", token)
 
+	// Extract id_token from the OAuth2 token
 	idToken, ok := token.Extra("id_token").(string)
 	if !ok {
-		// Fehlerbehandlung
+		// Handle missing id_token
 		log.Fatalln("id_token not found in token")
-
 		w.WriteHeader(http.StatusBadRequest)
 		_, err := w.Write([]byte("id_token not found in token"))
-
 		if err != nil {
 			log.Println("Error writing response:", err)
 		}
+		return
 	}
 
+	// Verify id_token
 	oidcConfig := &oidc.Config{
 		ClientID: clientID,
 	}
-	idTokenObj, err := s.provider.Verifier(oidcConfig).Verify(ctx, idToken)
+	idTokenObj, err := s.provider.Verifier(oidcConfig).Verify(s.ctx, idToken)
 	if err != nil {
-		// Fehlerbehandlung
+		// Handle verification error
 		return
 	}
 
-	// Nutzerdaten extrahieren
+	// Extract user claims from id_token
 	var claims struct {
 		Email string `json:"email"`
-		// weitere Felder
+		// Add more fields as needed
 	}
 	if err := idTokenObj.Claims(&claims); err != nil {
-		// Fehlerbehandlung
+		// Handle claim extraction error
 		return
 	}
 
+	// Respond with a greeting
 	w.WriteHeader(http.StatusOK)
 	_, err = w.Write([]byte("Hello " + claims.Email))
-
 	if err != nil {
 		log.Println("Error writing response:", err)
 	}
 }
 
+// main is the entry point of the application
 func main() {
-
 	log.Println("Starting webserver...")
 	defer log.Println("Stopping webserver...")
 
+	// Create a background context
 	ctx := context.Background()
 
+	// Initialize OIDC provider
 	provider, err := oidc.NewProvider(ctx, ProviderURL)
 	if err != nil {
 		panic(err)
 	}
 
+	// Initialize session with OAuth2 and OIDC configuration
 	sess := session{
+		ctx:      ctx,
 		provider: provider,
 		config: oauth2.Config{
 			ClientID:     clientID,
@@ -122,26 +126,16 @@ func main() {
 		},
 	}
 
-	// config := oauth2.Config{
-	// 	ClientID:     clientID,
-	// 	ClientSecret: clientSecret,
-	// 	Endpoint:     provider.Endpoint(),
-	// 	RedirectURL:  "http://localhost:" + fmt.Sprint(DefaultPort) + "/callback",
-	// 	Scopes:       []string{oidc.ScopeOpenID, "profile", "email"},
-	// }
-
+	// Define route for OAuth2 authorization
 	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, sess.config.AuthCodeURL(""), http.StatusFound)
 		log.Println("Redirecting to auth endpoint...")
 	})
 
+	// Define route for OAuth2 callback
 	http.HandleFunc("/callback", sess.handleAuthCallback)
 
-	// Start listening for HTTP requests
+	// Start the web server
 	log.Printf("Listening on http://localhost:%d/\n", DefaultPort)
 	log.Fatal(http.ListenAndServe(":"+fmt.Sprint(DefaultPort), nil))
 }
-
-// http://localhost:8080/callback
-//	?session_state=38ae7cba-1588-4671-83e0-4d123cd90bf6
-//	&code=0444e102-a71d-47ef-857a-8e6baa25e6fe.38ae7cba-1588-4671-83e0-4d123cd90bf6.228ac7b3-ceb7-4106-8a74-56d6708325c8
